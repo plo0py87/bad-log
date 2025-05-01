70b7b3d164baac2038e1a33546eff901792e1554
plo0py87 <ploopy0408@gmail.com>
2025-05-01 23:36:50 +0800

    修正netlify 部屬問題

--- fix-paths.js
+++ fix-paths.js
@@ -2,28 +2,35 @@
 import fs from 'fs';
 import path from 'path';
 
-console.log('Fixing asset paths in built files...');
+console.log('Preparing files for Netlify deployment...');
 
 // Read the index.html file
 const indexPath = path.resolve('./dist/index.html');
 let indexContent = fs.readFileSync(indexPath, 'utf8');
 
-// Replace absolute paths with relative ones
-indexContent = indexContent.replace(/src="\/react-blog\//g, 'src="./');
-indexContent = indexContent.replace(/href="\/react-blog\//g, 'href="./');
+// Only replace paths if they contain the specific pattern
+// This is now more conditional to avoid breaking working paths
+if (indexContent.includes('/react-blog/')) {
+  // Replace absolute paths with relative ones
+  indexContent = indexContent.replace(/src="\/react-blog\//g, 'src="./');
+  indexContent = indexContent.replace(/href="\/react-blog\//g, 'href="./');
+  
+  // Write the fixed content back
+  fs.writeFileSync(indexPath, indexContent);
+  console.log('Fixed asset paths in index.html');
+} else {
+  console.log('No path fixes needed in index.html');
+}
 
-// Write the fixed content back
-fs.writeFileSync(indexPath, indexContent);
-
-// Create proper _headers file
+// Create proper _headers file - this will override the one in your public folder
 const headersContent = `/*
   X-Frame-Options: DENY
   X-XSS-Protection: 1; mode=block
 `;
 fs.writeFileSync('./dist/_headers', headersContent);
 
-// Create proper _redirects file
+// Create proper _redirects file - this will override the one in your public folder
 const redirectsContent = '/* /index.html 200';
 fs.writeFileSync('./dist/_redirects', redirectsContent);
 
-console.log('Fixed asset paths and created Netlify configuration files.');
+console.log('Created Netlify configuration files.');
